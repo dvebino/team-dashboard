ac7f9e31b1e5736de724459f0dc970ad23c8415f
dvebino <dhuang2495@gmail.com>
2022-09-22 13:24:28 -0400

    created pages, index, + imports for App

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React, { useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { FiSettings } from "react-icons/fi";
 import { TooltipComponent } from "@syncfusion/ej2-react-popups";
-
+import { Navbar, Foot, Sidebar, Themes } from './components';
+import { Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kanban, Area, Bar, Pie, Financial, ColorTheme, ColorMapping, Editor} from './pages'
 import "./App.css";
 
 const App = () => {
